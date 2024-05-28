386f415a5ca6e49bf0201902411ed46e51f39b6c
Radhika5062 <radhikamaheshwari26@gmail.com>
2024-05-28 17:34:04 +1000

    Model Evaluation added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+from mlProject.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from mlProject.entity.config_entity import ModelTrainerConfig, ModelEvaluationConfig
 from mlProject import logger
 
 
@@ -92,3 +93,21 @@ class ConfigurationManager:
             target_column=schema.name
         )
         return model_trainer_config
+    
+    def get_model_evaluation_config(self)-> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            metric_file_name=config.metric_file_name,
+            all_params = params,
+            target_column = schema.name
+        )
+
+        return model_evaluation_config
